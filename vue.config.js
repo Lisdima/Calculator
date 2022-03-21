fceed746f2ebedb729db70463464342441ebd738
Dmitriy Lisyanskiy <d.lisyansky@efit.biz>
2022-03-21 14:47:25 +0300

    test webpack

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,10 @@ const path = require("path");
 
 module.exports = {
   publicPath: '',
+  configureWebpack: {
+    optimization: {
+      splitChunks: false // makes there only be 1 js file - leftover from earlier attempts but doesn't hurt
+    },
+  },
   transpileDependencies: true,
 };
